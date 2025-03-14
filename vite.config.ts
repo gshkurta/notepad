dd65ac5cf6cec4c42da95035e0b91769d3595c34
George Shkurta <g.shkurta@takt.gr>
2025-03-14 07:53:52 +0200

    feat: add UI components for form and card elements

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import unfonts from 'unplugin-fonts/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()],
+        },
     },
-  },
+    plugins: [
+        vue(),
+        vueDevTools(),
+        unfonts({
+            google: {
+                families: [
+                    { name: 'Manrope', styles: 'wght@200..800' }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
 })
